308c23f22d704432581ce452e179102d5fd93506
Shreyansh <shreyanshpadhiyar33@gmail.com>
2024-03-03 23:47:02 +0530

    Complate login and signup page, add base.html and extends it on all files

--- tixhub/urls.py
+++ tixhub/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path
 from django.urls import include
 from tixhub import view
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(("base.urls", "base"),"base"))
