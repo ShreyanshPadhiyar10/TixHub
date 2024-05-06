f13c304841cddfca324cfc1f036cb236782627f1
S X V V Y <utssavvpatel@gmail.com>
2024-05-06 20:12:01 +0530

    added more function

--- tixhub/view.py
+++ tixhub/view.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from base.models import Movie_details
 
 def home(req):
     return render(req, "index.html")
 
 
 def more_details(request):
-    movie_id = request.GET.get('movie_id')
+    # movie_det = Movie_details.objects.all()
+    # data = { 'movie_det' : movie_det }
+
+    movie_title = request.GET.get('movie_title')
+    movie = get_object_or_404(Movie_details, title = movie_title)
+
     
-    return render(request, 'more.html', {'movie_id': movie_id})
+    return render(request, 'more.html', {'movie' : movie})
 
